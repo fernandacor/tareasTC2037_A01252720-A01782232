8074d060698d0cfba9982c95352e53c7a5203f38
Fernanda Cantú <fercantu56@gmail.com>
2023-06-13 23:42:03 -0600

    borre un buen de cosas que probe y no funcionaron

--- programacionparalela/lib/Hw.Primes.ex
+++
@@ -1,8 +0,0 @@
-defmodule Hw.Prines do
-  def sum_primes do
-  end
-
-  def sum_primes_parallel do
-  end
-
-end
--- programacionparalela/test/programacionparalela_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule ProgramacionparalelaTest do
-  use ExUnit.Case
-  doctest Programacionparalela
-
-  test "greets the world" do
-    assert Programacionparalela.hello() == :world
-  end
-end
