cf498e2ea07e87579067bef42ccb4b399ebf1922
Fernanda Cantú <fercantu56@gmail.com>
2023-06-14 23:49:40 -0600

    ya sirve

--- Act5.2/Hw.Primes.exs
+++ Act5.2/Hw.Primes.exs
@@ -1,23 +1,6 @@
 # Actividad 5.2 - Programación Paralela y Concurrente
 # Fernanda Cantú A01782232 & Alina Rosas A01252720
-# defmodule Hw.Primes do
-#   def sum_primes(limit) do
-#   end
-#     # Recibe el limite de los numeros primos a sumar
-
-#     # Regresa la suma de los numeros primos menores al limite
-#     # Formula para figure out si un numero es un primo o no??
-#   defp is_prime(num)
-#   end
-
-#   def sum_primes_parallel(lim, thr) do
-
-#   end
-
-#   defp limits(inf, sup) do
-#   end
-
-# end
+# 14/06/2023
 
 defmodule Hw.Primes do
   def sum_primes(limit) do
@@ -30,7 +13,27 @@ defmodule Hw.Primes do
   end
 
   defp is_prime(n) do
-    for i <- 2..(n-1), rem(n, i) == 0, do: false
+    for i <- 2..trunc(:math.sqrt(n)), rem(n, i) == 0, do: false
     true
   end
-end
+
+  def sum_primes_parallel(limit, threads) do
+    ranges = make_ranges(limit, threads)
+    ranges
+    |> Enum.map(fn {start, stop} -> Task.async(fn -> range_sum(start, stop) end) end)
+    |> Enum.map(fn task -> Task.await(task) end)
+    |> Enum.sum()
+  end
+
+  defp make_ranges(limit, threads) do
+    step = div(limit, threads)
+    0..(threads - 1)
+    |> Enum.map(fn i -> {i * step + 1, min((i + 1) * step, limit)} end)
+  end
+
+  defp range_sum(start, stop) do
+    primes = find_primes(stop)
+    Enum.filter(primes, &(&1 >= start))
+    |> Enum.sum()
+  end
+  end
