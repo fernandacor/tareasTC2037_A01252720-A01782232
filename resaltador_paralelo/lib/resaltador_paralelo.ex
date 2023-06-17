21d59712aa8feb3932cf60adef21670bcec33504
Fernanda Cantú <fercantu56@gmail.com>
2023-06-16 21:06:13 -0600

    paralelo

---
+++ resaltador_paralelo/lib/resaltador_paralelo.ex
@@ -0,0 +1,18 @@
+defmodule ResaltadorParalelo do
+  @moduledoc """
+  Documentation for `ResaltadorParalelo`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ResaltadorParalelo.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ resaltador_paralelo/test/resaltador_paralelo_test.exs
@@ -0,0 +1,8 @@
+defmodule ResaltadorParaleloTest do
+  use ExUnit.Case
+  doctest ResaltadorParalelo
+
+  test "greets the world" do
+    assert ResaltadorParalelo.hello() == :world
+  end
+end
