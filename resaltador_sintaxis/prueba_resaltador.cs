28df7c0e1481eb160867f4fa9e50bbd2936616bd
Fernanda Cantú <fercantu56@gmail.com>
2023-06-19 10:19:41 -0600

    meti más cosas para probar que el lexer funciona

--- resaltador_sintaxis/prueba_resaltador.cs
+++ resaltador_sintaxis/prueba_resaltador.cs
@@ -1,3 +1,6 @@
+// Actividad Integradora 3.4 - Archivo de Prueba en C# para el Resaltador de Sintaxis
+// Fernanda Cantú A01782232 & Alina Rosas A01252720
+
 using System.Collections;
 using System.Collections.Generic;
 
@@ -7,10 +10,12 @@ using System.Collections.Generic;
 
 public class Prueba_Resaltador : MonoBehaviour
 {
-    public int = 0123;
-    private string = 'Hola Prof :D'
-    public float = 0.123;
+    public int num = 0123;
+    private string p4l4br4s = 'Hola Prof :D';
+    public float flotante = -0.123f;
+    public float flot2 = +898.765f;
     private bool granTrabajo = true;
+    protected char prueba = 'Fernanda 123' Alina 456; // Sabemos que esto no es un char, solo es para probar varias cosas
 
     void Start()
     {
@@ -22,12 +27,27 @@ public class Prueba_Resaltador : MonoBehaviour
     {
         if (granTrabajo == true)
         {
-            Debug.Log("¡¡Has hecho un gran trabajo!!");
+            Debug.Log('¡¡Has hecho un gran trabajo!!');
             return;
         }
         else
         {
-            Debug.Log("No te preocupes");
+            Debug.Log('No te preocupes, todo estará bien');
+        }
+
+        while (true)
+        {
+            do something;
+        }
+
+        switch (num)
+        {
+            case 0:
+                print('0');
+                break;
+            case 1:
+                print('1');
+                break;
         }
     }
 }
