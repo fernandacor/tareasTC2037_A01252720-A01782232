525cd4c5cc0608ec943e9be2fe6ff24e6a58180a
Fernanda Cantú <fercantu56@gmail.com>
2023-06-19 10:16:13 -0600

    ya está perfecto creo yo

--- resaltador_sintaxis/lib/resaltador_sintaxis.ex
+++ resaltador_sintaxis/lib/resaltador_sintaxis.ex
@@ -1,93 +1,83 @@
+# Actividad Integradora 3.4 - Resaltador de Sintaxis
+# Fernanda Cantú A01782232 & Alina Rosas A01252720
+
 defmodule ResaltadorSintaxis do
-  def build(input_file) do
+  # Función que recibe el nombre del archivo de entrada y construye el archivo de salida
+  def lexer(input_file) do
     output_file = "resaltadorsintaxis.html"
 
-    case File.read(input_file) do # Lee el archivo de cs
-      {:ok, content} -> #:ok = exito leyendo / content = donde se guarda el contenido del cs
-        lines = String.split(content, "\n") # Separa el contenido del cs por lineas
-        Enum.each(lines, &check_line/1) # Por cada linea, ejecuta la funcion check_line
+    File.write(output_file, header_html())
+    content = File.read!(input_file)
+    highlighter(content, output_file)
+    File.write(output_file, footer_html(), [:append])
+  end
 
-        highlighted_content =
-          content
-          |> highlight_strings()
-          |> highlight_comments()
-          |> highlight_punctuation()
-          |> highlight_numbers()
-          |> highlight_reserved()
-          |> highlight_cond()
-          |> highlight_builtinfunctions()
-          |> datatypes()
-          # |> highlight_variables()
-          # |> highlight_functions()
+  # Función para leer el documento línea por línea
+  defp highlighter(content, output_file) do
+    content
+    |> String.split("\n")
+    |> Enum.map(&String.trim_trailing/1)
+    |> Enum.map(fn line -> check_line(line, output_file) end)
+  end
 
-        html_content = header_html() <> highlighted_content <> footer_html()
+  # Función para checar cada línea y ver si hay coincidencias con las expresiones regulares
+  defp check_line("", output_file), do: File.write(output_file, "<br>", [:append])
+  defp check_line(line, output_file) do
+    comment_regex = ~r/^(\/\/.*$|\/\*.*?\*\/)/m
+    string_regex = ~r/^('[^']*')/
+    punctuation_regex = ~r/^(\(|\)|\{|\}|\[|\]|\.|\,|\;|\:|\=|\%|\<|\>)/
+    numbers_regex = ~r/^(\+|\-)?([0-9])(\.[0-9]+)?(f)?/
+    datatypes_regex = ~r/^\b(int|float|double|bool|char|long|string)\b/
+    reserved_regex = ~r/^(static|void|public|private|protected|true|false|class)/
+    cond_regex = ~r/^\b(using|if|else|for|case|switch|while|break|in|do|then|return)\b/
+    variable_regex = ~r/^[a-z_$][a-zA-Z0-9_$]*(?=(\.)*)/
+    id_regex = ~r/^([A-Z_$][a-zA-Z0-9_$]*)/
+    function_regex = ~r/^([a-zA-Z_$][a-zA-Z0-9_$]*(?=(\(|\<)))/
+    space_regex = ~r/^\s+/
+    other_regex = ~r/^\S+/
 
-        case File.write(output_file, html_content) do
-          :ok -> #if es exitoso:
-            IO.puts("HTML generated correctly.")
-        end
+    cond do
+      Regex.match?(punctuation_regex, line) ->
+        build(line, punctuation_regex, "punctuation", output_file)
+      Regex.match?(comment_regex, line) ->
+        build(line, comment_regex, "comment", output_file)
+      Regex.match?(string_regex, line) ->
+        build(line, string_regex, "string", output_file)
+      Regex.match?(reserved_regex, line) ->
+        build(line, reserved_regex, "reserved", output_file)
+      Regex.match?(cond_regex, line) ->
+        build(line, cond_regex, "cond", output_file)
+      Regex.match?(numbers_regex, line) ->
+        build(line, numbers_regex, "numbers", output_file)
+      Regex.match?(datatypes_regex, line) ->
+        build(line, datatypes_regex, "datatypes", output_file)
+      Regex.match?(function_regex, line) ->
+        build(line, function_regex, "functions", output_file)
+        Regex.match?(variable_regex, line) ->
+          build(line, variable_regex, "variables", output_file)
+      Regex.match?(id_regex, line) ->
+        build(line, id_regex, "id", output_file)
+          Regex.match?(space_regex, line) ->
+            build(line, space_regex, "space", output_file)
+      true ->
+        build(line, other_regex, "other", output_file)
     end
   end
 
-  defp check_line(line) do
-  defp highlight_punctuation(content) do
-    punctuation_regex = ~r/(\(|\)|\{|\}|\[|\]|\,|\;|\:|\=)/
-    Regex.replace(punctuation_regex, content, "<span class=\"punctuation\">\\1</span>")
+  # Función para construir el archivo de salida
+  defp build(line, regex, class, output_file) do
+    [hd|_] = Regex.run(regex, line)
+    span_replace = "<span class=\"#{class}\">#{hd}</span>"
+    File.write(output_file, span_replace, [:append])
+    line = Regex.replace(regex, line, "", global: false)
+    check_line(line, output_file)
   end
 
-  defp highlight_comments(content) do
-    comment_regex = ~r/^(\/\/.*$|\/\*.*?\*\/)/m
-    Regex.run(~r/(int|do)/, content fn m -> "<span class=\"comment\">\\1</span>" end)
-    Regex.replace(comment_regex, content, "<span class=\"comment\">\\1</span>")
-  end
-
-  defp highlight_strings(content) do
-    string_regex = ~r/('[^']*')/
-    Regex.replace(string_regex, content, "<span class=\"string\">\\1</span>")
-  end
-
-
-  defp highlight_reserved(content) do
-    reserved_regex = ~r/(static|void|public|private|protected|MonoBehaviour|public class|private class)/
-    Regex.replace(reserved_regex, content, "<span class=\"reserved\">\\1</span>")
-  end
-
-  defp highlight_cond(content) do
-    cond_regex = ~r/(using|if|else|for|case|switch|while|break)/
-    Regex.replace(cond_regex, content, "<span class=\"cond\">\\1</span>")
-  end
-
-  defp highlight_builtinfunctions(content) do
-    builtinfunctions_regex = ~r/(Start|Update|Play|GetComponent|FindGameObjectWithTag|OnTriggerEnter2D|Log|SetBool|Destroy|SetGameState)/
-    Regex.replace(builtinfunctions_regex, content, fn match -> "<span class=\"builtinfunctions\">#{match}</span>" end)
-  end
-
-  defp highlight_numbers(content) do
-    numbers_regex = ~r/(\+|\-)?([0-9])(\.[0-9]+)?(f)?/
-    Regex.replace(numbers_regex, content, "<span class=\"numbers\">\\1\\2\\3\\4</span>")
-  end
-
-  defp datatypes(content) do
-    datatypes_regex = ~r/(int|float|double|bool|char|long|GameManager|AudioSource|Animator|GameObject|string|Sprite|Image)/
-    Regex.replace(datatypes_regex, content, "<span class=\"datatypes\">\\1</span>")
-  end
-
-  #FUNCTION TO HIGHLIGHT VARIABLES
-  # defp highlight_variables(content) do
-  #  variables_regex = ~r/([a-z]+[0-9]*)/
-  #  Regex.replace(variables_regex, content, "<span class=\"variables\">\\1</span>")
-  # end
-
-  # #FUNCTION TO HIGHLIGHT FUNCTION NAMES
-  # defp highlight_functions(content) do
-  #  functions_regex = ~r/([a-z]+[0-9]*)/
-  #  Regex.replace(functions_regex, content, "<span class=\"functions\">\\1</span>")
-  # end
-
+  # Funcion que contiene el header del documento HTML
   defp header_html() do
     """
     <!DOCTYPE html>
-    <html>
+      <html>
       <head>
         <meta charset="UTF-8">
         <title>Resaltador de Sintaxis</title>
@@ -97,15 +87,16 @@ defmodule ResaltadorSintaxis do
       <h1>Resaltador de Sintaxis para C#</h1>
       <h3> Creado por: Fernanda Cantú A01782232 y Alina Rosas A01252720</h3>
       <hr>
-        <pre>
+      <pre>
     """
   end
 
+  # Funcion que contiene el footer del documento HTML
   defp footer_html() do
     """
-        </pre>
+      </pre>
       </body>
-    </html>
+      </html>
     """
   end
 end
