2232c3f9162ddaf67bbad0aa8bfd8304307dda6c
Fernanda Cantú <fercantu56@gmail.com>
2023-11-26 13:01:34 -0600

    empezar evidencia 2 - TC2038

---
+++ Evidencia2_TC2038/actividadIntegradora2.cpp
@@ -0,0 +1,59 @@
+// Evidencia 2: Actividad Integradora
+// Alina Rosas Macedo A01252720 y Fernanda Cantú Ortega A01782232
+// 30/11/2023
+
+//Líbrerias a utilizar
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+// Leer un archivo de entrada que contiene la información de un grafo representado en forma de una matriz de adyacencia
+// con grafos ponderados. El peso de cada arista es la distancia en km entre colonia y colonia, por donde se puede meter cableado
+void leerArchivo()
+{
+    return;
+}
+
+// El programa debe desplegar cuál es la forma optima de cablear con fibra optica conectando colonias de tal forma que se
+// pueda compartir información entre cuales quiera dos colonias
+void cableadoOptimo()
+{
+    return;
+}
+
+// Cuál es la ruta más corta posible que visita cada colonia una vez y al finalizar regresa a la colonia origen?
+// El programa debe desplegar la ruta a considerar, tomando en cuenta que la primera ciudad es A, la segunda B, y así
+// La empresa quiere conocer el flujo máximo de información del nodo inicial al nodo final. Esto tmbn debe desplegarse
+void shortestRoute()
+{
+    return;
+}
+
+// Teniendo en cuenta la ubicación geográfica de varias centrales a las que se pueden conectar nuevas casas, la empresa quiere
+// contar con una forma de decidir, dada una nueva contratación del servicio, cuál es la central geográficamente más cercana
+// a esa nueva contratación. No necesariamente hay una central por cada colonia. Se pueden tener colonias sin central, y colonias
+// con más de una central
+void centralCercana()
+{
+    return;
+}
+
+// Imprimir todo
+void imprimir()
+{
+    return;
+}
+
+// Función principal
+int main()
+{
+    leerArchivo();
+    cableadoOptimo();
+    shortestRoute();
+    centralCercana();
+    imprimir();
+}
+
